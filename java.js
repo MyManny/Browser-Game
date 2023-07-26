3692782580c7eef7929127cab5ebb7cb443ee624
MyManny <gmail.com>
2023-07-25 19:49:20 -0700

    fifth

---
+++ java.js
@@ -0,0 +1,17 @@
+// linking Divs and setting genres and questions
+
+const game = document.getElementById('game')
+const scoreDisplay = document.getElementById('score')
+
+const jeopardyCategories = [
+    {
+        genre: "WHO",
+        questions: [
+
+            
+        ]
+
+    }
+
+
+]
