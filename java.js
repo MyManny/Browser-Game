fbc6a49be6c1593292538147287eaab5af7f7090
MyManny <gmail.com>
2023-07-25 20:14:39 -0700

    set up question layout

--- java.js
+++ java.js
@@ -2,16 +2,134 @@
 
 const game = document.getElementById('game')
 const scoreDisplay = document.getElementById('score')
-
+// categories who what where why and how or more specific if I decide to do a certain theme
 const jeopardyCategories = [
-    {
-        genre: "WHO",
+    
+        // setting questions format 3 questions per category, easy medium hard
+    {   
+        genre: "",
         questions: [
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
 
-            
-        ]
+        ],
 
-    }
+    },
+    {   
+        genre: "",
+        questions: [
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
 
+        ],
+
+    },
+    {   
+        genre: "",
+        questions: [
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+
+        ],
+
+    },
+    {   
+        genre: "",
+        questions: [
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+
+        ],
+
+    },
+    {   
+        genre: "",
+        questions: [
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+            {
+                Question:"",
+                answers: ["",""],
+                correct:"",
+                level:"",
+            },
+
+        ],
+
+    },
 
 ]
