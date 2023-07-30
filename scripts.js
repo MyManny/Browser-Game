13a8db3f913a5e401925003ae21ba9945a26d4f6
MyManny <gmail.com>
2023-07-30 13:48:24 -0700

    created generes and set up next steps

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
 const game = document.getElementById('game')
 const scoreDisplay = document.getElementById('score')
 let score = 0
+// create genres, level of difficulty
+// create tables with javascript link to scores
+// fetch premade Api 
+// create flipcard Effect,results 
+const genres = [
+    {
+    name: 'Books',
+    id: 10
+    },
+    {
+    name: 'Film',
+    id: 11
+    },
+    {
+    name: 'Music',
+    id: 12
+    },
+    {
+    name: 'Video Games',
+    id: 15
+    }
+]
